8b6308b64e691dfa2b0ce26f4724727e2488714b
Rumki Adak <rumkiadak001@gmail.com>
2025-03-23 17:59:08 +0530

    Initial commit

---
+++ src/app/_components/Footer.js
@@ -0,0 +1,10 @@
+
+
+const Footer=()=>{
+    return(
+        <div className="footer-wrapper">
+            <p> All Rights Reserved By Next Food</p>
+        </div>
+    )
+}
+export default Footer;
